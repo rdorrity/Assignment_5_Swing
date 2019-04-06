00e9c94b61de94236e0ddd870f9619bdeb8ab865
Ryan <rdorrity@gmail.com>
2019-04-05 19:13:53 -0700

    Added CardTable class

--- src/CardTable.java
+++ src/CardTable.java
@@ -1,3 +1,19 @@
-public class CardTable
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class CardTable extends JFrame
 {
+    static int MAX_CARDS_PER_HAND = 56;
+    static int MAX_PLAYERS = 2;
+
+    private int numCardsPerHand;
+    private int numPlayers;
+
+    public JPanel pnlComputerHand, getPnlHumanHand, pnlPlayArea;
+
+    public CardTable()
+    {
+
+    }
 }
